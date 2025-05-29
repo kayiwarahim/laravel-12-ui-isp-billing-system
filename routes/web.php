59865fcd15a3fd853693aa4d075792edd89ec58b
kayiwarahim <kayiwarahim@gmail.com>
2025-05-29 16:46:52 +0300

    Remove unused routes and controllers for Tickets and Leads

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@ Route::get('/', function () {
 })->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
+    Route::get('dashboard', function () {return Inertia::render('dashboard');})->name('dashboard');
 
     Route::prefix('routers')->name('routers.')->group(function () {
         Route::get('/', [RouterController::class, 'index'])->name('index');
