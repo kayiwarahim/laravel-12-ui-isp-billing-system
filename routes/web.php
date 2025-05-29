527d96cb234131ec5c998c608997b7478cc80ec0
kayiwarahim <kayiwarahim@gmail.com>
2025-05-29 16:47:47 +0300

    Refactor routes by removing unused TicketController import

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\RouterController;
-use App\Http\Controllers\TicketController;
 use App\Http\Controllers\UserController as UserManagementController;
 
-Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
+Route::get('/', function () {return Inertia::render('welcome');})->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {return Inertia::render('dashboard');})->name('dashboard');
